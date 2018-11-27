7fde9f8b6d5e260d82b82240cb7aa209b3cc845a
ggr <ginagonzalezroundey@gmail.com>
2018-11-27 15:40:02 -0500

    all page functionality complete

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-	constructor() { }
+	timeinterval;
+	daysSpan;
+	hoursSpan;
+	minutesSpan;
+	secondsSpan;
 
-	ngOnInit() {
+	constructor() { 
+
 	}
 
+	ngOnInit() {
+		this.updateClock();
+		this.timeinterval = setInterval(this.updateClock, 1000);
+	}
+
+	updateClock() {
+		var clock = document.getElementById('clockdiv');
+		this.daysSpan = clock.querySelector('.days');
+		this.hoursSpan = clock.querySelector('.hours');
+		this.minutesSpan = clock.querySelector('.minutes');
+		this.secondsSpan = clock.querySelector('.seconds');
+
+		var diff = new Date(2019, 6, 27, 0, 0, 0, 0).getTime() - new Date().getTime();
+		var seconds = Math.floor((diff / 1000) % 60);
+		var minutes = Math.floor((diff / 1000 / 60) % 60);
+		var hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		var t = {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+
+		this.daysSpan.innerHTML = t.days;
+		this.hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		this.minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		this.secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+		if (t.total <= 0) {
+			clearInterval(this.timeinterval);
+		}
+	}
 }
