b556c00c3e69c83b7b90f7ab4e121b4fee114eec
ggr <ginagonzalezroundey@gmail.com>
2020-02-12 11:49:06 -0500

    show jobs; add registration button

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
-	selector: 'app-board',
-	templateUrl: './board.component.html',
-	styleUrls: ['./board.component.css']
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
 
-	constructor(private titleService: Title) {
-		this.titleService.setTitle('Meet the Camp It Up! Board of Directors');
-	}
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Meet the Camp It Up! Board of Directors');
+  }
 
-	ngOnInit() {
-	}
+  ngOnInit() {
+  }
 
 }
