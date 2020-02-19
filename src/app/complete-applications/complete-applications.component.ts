b99b99c94f9e542113b3e3c8968ec6fc721662e5
ggr <ginagonzalezroundey@gmail.com>
2020-02-19 11:17:16 -0500

    update application view; change 2019 to 2020

--- src/app/complete-applications/complete-applications.component.ts
+++ src/app/complete-applications/complete-applications.component.ts
@@ -1,48 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, Params } from '@angular/router';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-	selector: 'app-complete-applications',
-	templateUrl: './complete-applications.component.html',
-	styleUrls: ['./complete-applications.component.css']
+  selector: 'app-complete-applications',
+  templateUrl: './complete-applications.component.html',
+  styleUrls: ['./complete-applications.component.css']
 })
 export class CompleteApplicationsComponent implements OnInit {
 
-	applications: any;
-	registerForm: FormGroup;
+  applications: any;
+  registerForm: FormGroup;
+  show = 2020;
 
-	constructor(private router: Router, private db: AngularFirestore, private afAuth: AngularFireAuth, private fb: FormBuilder) {
-		this.registerForm = this.fb.group({
-			email: ['', [Validators.required, Validators.email]],
-			password: ['', Validators.required],
-			confirmPassword: ['', Validators.required]
-		}, {
-				validator: [this.mustMatch('password', 'confirmPassword')]
-			});
+  constructor(private db: AngularFirestore, private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: [this.mustMatch('password', 'confirmPassword')]
+    });
 
-		this.db.collection('applications', ref => ref.orderBy('timestamp', 'desc')).valueChanges()
-			.subscribe(doc => { this.applications = doc.slice().reverse(); });
-	}
+    this.db.collection('applications', ref => ref.orderBy('timestamp', 'desc')).valueChanges()
+      .subscribe(doc => { this.applications = doc.slice().reverse(); console.log(this.applications[0].timestamp) });
+  }
 
-	ngOnInit() { }
+  ngOnInit() { }
 
-	tryRegister(value) {
-		firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
-			.then(_ => alert('added new profile'), err => console.error('error', err));
-	}
+  tryRegister(value) {
+    firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+      .then(_ => alert('added new profile'), err => console.error('error', err));
+  }
 
-	mustMatch(controlName: string, matchingControlName: string) {
-		return (formGroup: FormGroup) => {
-			const control = formGroup.controls[controlName];
-			const matchingControl = formGroup.controls[matchingControlName];
-			if (matchingControl.errors && !matchingControl.errors.mustMatch) { return; }
-			if (control.value !== matchingControl.value) {
-				matchingControl.setErrors({ mustMatch: true });
-			} else { matchingControl.setErrors(null); }
-		};
-	}
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) { return; }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else { matchingControl.setErrors(null); }
+    };
+  }
 }
